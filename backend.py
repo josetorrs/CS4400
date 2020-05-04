281b6c76e6a8ab1d4177325bf4ae63ef2c9d6fce
Jose Torres <josetorrs7@gmail.com>
2020-05-04 18:40:50 -0500

    formatting.

--- backend.py
+++ backend.py
@@ -1,34 +1,32 @@
+from sqlite3 import connect
 from twitterscraper.query import query_tweets
 from pandas import DataFrame
 from afinn import Afinn
-from sqlite3 import connect
 
 
-def get_tweets(query, begindate, enddate):
-    
+def get_tweets(query, begin_date, end_date):
     lang = 'english'
     limit = None
     filters = ['tweet_id', 'text', 'timestamp', 'likes', 'retweets', 'user_id', 'screen_name']
     tweets = None
-    
-    if begindate is not None and enddate is not None:
-        tweets = query_tweets(query, begindate=begindate, enddate=enddate, lang=lang, limit=limit)
-    elif begindate is not None:
-        tweets = query_tweets(query, begindate=begindate, lang=lang, limit=limit)
-    elif enddate is not None:
-        tweets = query_tweets(query, enddate=enddate, lang=lang, limit=limit)
+
+    if begin_date is not None and end_date is not None:
+        tweets = query_tweets(query, begin_date=begin_date, end_date=end_date, lang=lang, limit=limit)
+    elif begin_date is not None:
+        tweets = query_tweets(query, begin_date=begin_date, lang=lang, limit=limit)
+    elif end_date is not None:
+        tweets = query_tweets(query, end_date=end_date, lang=lang, limit=limit)
     else:
         tweets = query_tweets(query, lang=lang, limit=limit)
-    
-    dataframe = DataFrame(tweet.__dict__ for tweet in tweets)[filters]
-    dataframe.dropna()
-    return dataframe
+
+    data_frame = DataFrame(tweet.__dict__ for tweet in tweets)[filters]
+    data_frame.dropna()
+    return data_frame
 
 
 def ensure_tables(connection):
-    
-    cursor = connection.cursor()
-    cursor.executescript("""CREATE TABLE IF NOT EXISTS Handle (
+	cursor = connection.cursor()
+	cursor.executescript("""CREATE TABLE IF NOT EXISTS Handle (
                                 HandleId INTEGER NOT NULL PRIMARY KEY,
                                 Username TEXT NOT NULL UNIQUE
                             );
@@ -63,63 +61,62 @@ def ensure_tables(connection):
 
 
 def insert_tweets(connection, tweets, query):
-    
-    cursor = connection.cursor()
-    analysis = Afinn()
-    
-    for index, tweet in tweets.iterrows():
-        
-        sql = "INSERT INTO Handle(HandleId, Username) VALUES(?, ?);"
-        values = (tweet['user_id'], tweet['screen_name'])
-        try:
-            cursor.execute(sql, values)
-        except:
-            pass # repeat entry
-        
-        sentiment = analysis.score(tweet['text'])
-        stamp = tweet['timestamp'].to_pydatetime()
-        
-        sql = "INSERT INTO Tweet(TweetId, Post, Sentiment, Stamp, NumLikes, NumRetweets, HandleId) VALUES(?, ?, ?, ?, ?, ?, ?);"
-        values = (tweet['tweet_id'], tweet['text'], sentiment, stamp, tweet['likes'], tweet['retweets'], tweet['user_id'])
-        try:
-            cursor.execute(sql, values)
-        except:
-            pass # repeat entry
-    
-    sql = "INSERT INTO Query(Topic, StartTime, EndTime, MinLikes, MinRetweets) VALUES(?, ?, ?, ?, ?);"
-    values = query
-    cursor.execute(sql, values)
-    
-    queryid = cursor.lastrowid
-    
-    sql = """   INSERT INTO Sampled(QueryId, TweetId)
-                    SELECT ?, TweetId FROM Tweet, Query WHERE (QueryId = ?)
+	cursor = connection.cursor()
+	analysis = Afinn()
+
+	for _, tweet in tweets.iterrows():
+
+		sql = "INSERT INTO Handle(HandleId, Username) VALUES(?, ?);"
+		values = (tweet['user_id'], tweet['screen_name'])
+		try:
+			cursor.execute(sql, values)
+		except:
+			pass  # repeat entry
+
+		sentiment = analysis.score(tweet['text'])
+		stamp = tweet['timestamp'].to_pydatetime()
+
+		sql = "INSERT INTO Tweet(TweetId, Post, Sentiment, Stamp, NumLikes, NumRetweets, HandleId) "\
+			  "VALUES(?, ?, ?, ?, ?, ?, ?); "
+		values = (tweet['tweet_id'], tweet['text'], sentiment, stamp,
+				  tweet['likes'], tweet['retweets'], tweet['user_id'])
+		try:
+			cursor.execute(sql, values)
+		except:
+			pass  # repeat entry
+
+	sql = "INSERT INTO Query(Topic, StartTime, EndTime, MinLikes, MinRetweets) "\
+		  "VALUES(?, ?, ?, ?, ?);"
+	values = query
+	cursor.execute(sql, values)
+
+	query_id = cursor.lastrowid
+
+	sql = """   INSERT INTO Sampled(QueryId, TweetId) 
+				SELECT ?, TweetId FROM Tweet, Query WHERE (QueryId = ?)
                     AND (LOWER(Post) LIKE ('%' || LOWER(Topic) || '%'))
                     AND ((Tweet.Stamp >= StartTime) OR (StartTime IS NULL))
                     AND ((Tweet.Stamp <= EndTime) OR (EndTime IS NULL))
                     AND ((NumLikes >= MinLikes) OR (MinLikes IS NULL))
                     AND ((NumRetweets >= MinRetweets) OR (MinRetweets IS NULL));   """
-    values = (queryid, queryid)
-    cursor.execute(sql, values)
-    
-    return queryid
+	values = (query_id, query_id)
+	cursor.execute(sql, values)
+
+	return query_id
 
 
-def analyze_tweets(connection, queryid):
-    
-    #cursor = connection.cursor()
-    return None
+def analyze_tweets(connection, query_id):
+	# cursor = connection.cursor()
+	return None
 
 
-def main(topic='Trump', begindate=None, enddate=None, minlikes=None, minretweets=None):
-    
-    tweets = get_tweets(topic, begindate, enddate)
-    with connect('project.db') as connection:
-        ensure_tables(connection)
-        queryid = insert_tweets(connection, tweets, (topic, begindate, enddate, minlikes, minretweets))
-        analyze_tweets(connection, queryid)
+def main(topic='Trump', begin_date=None, end_date=None, min_likes=None, min_retweets=None):
+	tweets = get_tweets(topic, begin_date, end_date)
+	with connect('project.db') as connection:
+		ensure_tables(connection)
+		query_id = insert_tweets(connection, tweets, (topic, begin_date, end_date, min_likes, min_retweets))
+		analyze_tweets(connection, query_id)
 
 
 if __name__ == '__main__':
 	main()
-
--- projectFrontEnd.py
+++ projectFrontEnd.py
@@ -3,53 +3,59 @@ from backend import get_tweets
 from datetime import datetime
 from tkcalendar import Calendar, DateEntry
 
+
 class QueryGUI:
-        
-    def __init__(self,root):
-        self.root = root
-        root.title("Twitter Scraper")
-        self.currDate = datetime.today()
-        self.useCurrDate = tk.BooleanVar()
-        self.useCurrDate.set(False)
-        
-        self.keywordFrame = tk.Frame(root)
-        self.keywordFrame.pack(padx=50)
-        self.rootTxtBxLabel = tk.Label(self.keywordFrame, text="Keyword").pack()
-        
-        self.myQueryVar = tk.StringVar()
-        self.myQuery = tk.Entry(self.keywordFrame, bd=1, width=40, textvariable=self.myQueryVar).pack()
-        
-        self.dateFrame = tk.Frame(root)
-        
-        self.dateFrame.pack(pady=10)
-        self.beginDateLabel = tk.Label(self.dateFrame,text="Choose Begin Date").pack(padx=5,pady=5)
-        self.beginDateVar = tk.StringVar()
-        self.beginCalendar = DateEntry(self.dateFrame,date_pattern="mm-dd-yyyy", maxdate=datetime.today(),mindate=datetime(2006,3,21), textvariable=self.beginDateVar).pack()
-        
-        self.endDateLabel = tk.Label(self.dateFrame,text="Choose End Date").pack()
-        self.endDateVar = tk.StringVar()
-        self.endCalendar = DateEntry(self.dateFrame,date_pattern="mm-dd-yyyy", maxdate=datetime.today(),mindate=datetime(2006,3,21),textvariable=self.endDateVar).pack(pady=5)
-        
-        self.sendQueryButton = tk.Button(self.dateFrame, text="Send Query",command=self.getScraperData).pack()
-        self.quitButton = tk.Button(self.dateFrame,text="Quit",command=self.close_window).pack(pady=5)
-    
-    def getScraperData(self):
-        beginDateStr = self.beginDateVar.get()
-        beginDate = datetime.strptime(beginDateStr,"%m-%d-%Y")
-        endDateStr = self.endDateVar.get()
-        endDate = datetime.strptime(endDateStr,"%m-%d-%Y")
-        myQuery = self.myQueryVar.get()
-        get_tweets(myQuery,beginDate.date(),endDate.date())
-    def close_window(self):
-        self.root.destroy()
-    
-    '''ef set_Bool(self):
+
+	def __init__ (self, root):
+		self.root = root
+		root.title("Twitter Scraper")
+		self.currDate = datetime.today()
+		self.useCurrDate = tk.BooleanVar()
+		self.useCurrDate.set(False)
+
+		self.keywordFrame = tk.Frame(root)
+		self.keywordFrame.pack(padx = 50)
+		self.rootTxtBxLabel = tk.Label(self.keywordFrame, text = "Keyword").pack()
+
+		self.myQueryVar = tk.StringVar()
+		self.myQuery = tk.Entry(self.keywordFrame, bd = 1, width = 40, textvariable = self.myQueryVar).pack()
+
+		self.dateFrame = tk.Frame(root)
+
+		self.dateFrame.pack(pady = 10)
+		self.beginDateLabel = tk.Label(self.dateFrame, text = "Choose Begin Date").pack(padx = 5, pady = 5)
+		self.beginDateVar = tk.StringVar()
+		self.beginCalendar = DateEntry(self.dateFrame, date_pattern = "mm-dd-yyyy", maxdate = datetime.today(),
+									   mindate = datetime(2006, 3, 21), textvariable = self.beginDateVar).pack()
+
+		self.endDateLabel = tk.Label(self.dateFrame, text = "Choose End Date").pack()
+		self.endDateVar = tk.StringVar()
+		self.endCalendar = DateEntry(self.dateFrame, date_pattern = "mm-dd-yyyy", maxdate = datetime.today(),
+									 mindate = datetime(2006, 3, 21), textvariable = self.endDateVar).pack(pady = 5)
+
+		self.sendQueryButton = tk.Button(self.dateFrame, text = "Send Query", command = self.getScraperData).pack()
+		self.quitButton = tk.Button(self.dateFrame, text = "Quit", command = self.close_window).pack(pady = 5)
+
+	def getScraperData (self):
+		beginDateStr = self.beginDateVar.get()
+		beginDate = datetime.strptime(beginDateStr, "%m-%d-%Y")
+		endDateStr = self.endDateVar.get()
+		endDate = datetime.strptime(endDateStr, "%m-%d-%Y")
+		myQuery = self.myQueryVar.get()
+		get_tweets(myQuery, beginDate.date(), endDate.date())
+
+	def close_window (self):
+		self.root.destroy()
+
+	'''ef set_Bool(self):
         self.useCurrDate.get()'''
 
-def main():
-    root = tk.Tk()
-    myQueryGUI = QueryGUI(root)
-    root.mainloop()
-    
+
+def main ():
+	root = tk.Tk()
+	myQueryGUI = QueryGUI(root)
+	root.mainloop()
+
+
 if __name__ == "__main__":
-    main()
+	main()
